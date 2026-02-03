aee7cd705589a8e5dfca71fe85db76f347f36164
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2026-02-03 14:20:36 +0100

    Update utils.py

--- src/models/utils.py
+++ src/models/utils.py
@@ -3,12 +3,15 @@ import torch
 
 from enum import IntEnum
 
+# =============================
+# === Points tranformations ===
+# =============================
 
 class projection_type(IntEnum):
     POLAR2CARTESIAN = 0
     CARTESIAN2POLAR = 1
 
-def project_points(pts, mode:projection_type):
+def transorm_points_coords(pts, mode:projection_type):
     '''
 
     :pt: numpy array with points
@@ -45,61 +48,77 @@ def project_points(pts, mode:projection_type):
         phi = np.arctan2(z, np.sqrt(x**2 + y**2))
 
         return np.column_stack((r, theta, phi))
-
-
-def affine_matrix(state, invert = False):
-    """
-    Generates a 4x4 homogeneous transformation matrix.
-    state: (x, y, z, roll, pitch, yaw) in radians.
-
-    if invert == True, returns also inverse affine matrix
-    """
-    x, y, z, roll, pitch, yaw = state
-
-    # Precompute sines and cosines for efficiency
-    cr, sr = np.cos(roll),  np.sin(roll)
-    cp, sp = np.cos(pitch), np.sin(pitch)
-    cy, sy = np.cos(yaw),   np.sin(yaw)
-
-    # Construct the matrix T (ZYX convention)
-    T = np.array([
-        [cy*cp, cy*sp*sr - sy*cr, cy*sp*cr + sy*sr, x],
-        [sy*cp, sy*sp*sr + cy*cr, sy*sp*cr - cy*sr, y],
-        [-sp,   cp*sr,            cp*cr,            z],
-        [0,     0,                0,                1]
-    ])
-
-    if invert:
-        R = T[:3, :3]
-        t = T[:3, 3]
-
-        # Invert rotation 
-        R_inv = R.T
         
-        # Invert translation: -R^T * t
-        t_inv = -R_inv @ t
+def transform_matrix(state):
+    x, y, z, qx, qy, qz, qw = state # quaterions 
 
-        # Assemble the inverted matrix
-        T_inv = np.eye(4)
-        T_inv[:3, :3] = R_inv
-        T_inv[:3, 3] = t_inv
-        return T, T_inv
-    else:
-        return T
+    # pre-calculation
+    xx = qx * qx
+    yy = qy * qy
+    zz = qz * qz
+    xy = qx * qy
+    xz = qx * qz
+    yz = qy * qz
+    wx = qw * qx
+    wy = qw * qy
+    wz = qw * qz
 
+    # compose translation matrix
+    row0 = torch.stack([1 - 2*(yy + zz),     2*(xy - wz),     2*(xz + wy),    x])
+    row1 = torch.stack([    2*(xy + wz), 1 - 2*(xx + zz),     2*(yz - wx),    y])
+    row2 = torch.stack([    2*(xz - wy),     2*(yz + wx), 1 - 2*(xx + yy),    z])
+    row3 = torch.tensor([           0.0,             0.0,             0.0,  1.0], device=state.device, dtype=state.dtype)
+ 
+    T = torch.stack([row0, row1, row2, row3])
+    return T
 
-def transform_points(x, T):
+def inverse_transform_matrix(T):
+
+    # extract rotation and shift 
+    R = T[:3, :3]
+    t = T[:3, 3]
+
+    # inverse components
+    R_inv = R.T
+    t_inv = - torch.mv(R_inv, t)
+
+    # compose inverse matrix 
+    T_inv = torch.zeros((4,4), device = T.device, dtype = T.dtype)
+    
+    T_inv[:3, :3] = R_inv
+    T_inv[:3, 3] = t_inv
+    T_inv[3, :] = torch.tensor([0.0, 0.0, 0.0, 1.0], device = T.device, dtype = T.dtype)
+    return T_inv 
+    
+def project_points(origin_pt, origin_pose, target_pose):
+
     '''
-    Transform points with rotation and shift defined in affine matrix T
-    :x: torch tensor of points, shape (n, 4):  [x1, y1, z1, 1;
-                                                x2, y2, z2, 1; 
-                                                xn, yn, zn, 1]
-    :T: affine matrix, shape (4,4)
+    Calculate projection of point from origin patch to target patch. 
+    Note: origin_pt shall be already scaled to real-world values, not in pixels. 
     '''
-    x_t = T @ x.T
-    return x_t.T
+    # --- Project origin point from spehrical to cartesian coords sys. (r, theta, phi) -> (x, y, z) ---
+    origin_pt_xyz = transorm_points_coords(origin_pt, projection_type.POLAR2CARTESIAN)
+    origin_pt_xyz = origin_pt_xyz.T 
+
+    # --- Compose relative translation matrix from origin pose to target pose --- 
+    T_origin = transform_matrix(origin_pose)
+    T_target = transform_matrix(target_pose)
+    T_target_inv = inverse_transform_matrix(T_target)
+    T_relative = T_target_inv @ T_target
+
+    # --- Calculate translated point coords: Origin pose (x1, y1, z1) -> Target pose (x2, y2, z2) ---
+    target_pt_xyz = T_relative * origin_pt_xyz
+    target_pt_xyz = target_pt_xyz.T
+
+    # --- Project target point to spherical coords system  ---
+    target_pt = transorm_points_coords(target_pt_xyz, projection_type.CARTESIAN2POLAR)
+    
+    return target_pt
 
 
+# =======================
+# === Quaterions math ===
+# =======================
 
 def q_conjugate(q):
     # quaterion conjugate
