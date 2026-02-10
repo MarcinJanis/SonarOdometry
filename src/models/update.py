6915841228b0f75382118c2bbf097a5a54ddd365
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2026-02-10 10:16:18 +0100

    Add model configuration and correlation network setup
    Initialize model configuration parameters and set up correlation network.

--- src/models/update.py
+++ src/models/update.py
@@ -10,6 +10,20 @@ class Update(nn.Module):
     def __init__(self, model_cfg):
         super().__init__()
 
+        # --- get model configuration ---
+        self.fmap_c = self.model_cfg.FEATURES_OUTPUT_CH
+        self.corr_neighbour = self.model_cfg.CORR_NEIGHBOUR
+        self.patch_size = self.model_cfg.PATCH_SIZE 
+        
+        # correlation preprocess net
+        corr_input_dim = self.fmap_c*self.corr_neighbour*self.corr_neighbour*self.patch_size*self.patch_size
+        corr_output_dim = self.model_cfg. #TODO
+        self.corr_net = nn.Sequential(
+            nn.Linear(corr_input_dim, corr_output_dim)
+        )
+
+   
+        
 
     
     def forward():
