db72d9f34641d82da445d6cef542fdbb2cce1d8c
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2026-02-23 13:45:05 +0100

    Fix typos and update variable names in bundle_adjustment

--- src/models/bundle_adjustment.py
+++ src/models/bundle_adjustment.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .utils import transorm_points_coords, transform_matrix, projection_type
+from .utils import transorm_points_coords, projection_type, transform_matrix
 
 import pypose as pp
 
 
-class BoundleAdjustment(nn.Module):
+class BundleAdjustment(nn.Module):
     def __init__(self, poses, patch_coords):
         super().__init__()
 
@@ -37,9 +37,10 @@ class BoundleAdjustment(nn.Module):
             target_coords = torch.cat([patch_coords, elevation_angle], dim = 1)
             
             # --- transform points --- 
-            T = transform_matrix(poses)
-            target_coords  = T @ target_coords # złożyć target coords z tych dóch wcześniejsyzch 
-            target_coords = transorm_points_coords(target_coords, projection_type.CARTESIAN2POLAR)
+            # T = transform_matrix(poses)
+            # target_coords  = T @ target_coords # złożyć target coords z tych dóch wcześniejsyzch 
+            target_coords = poses @ proj_coords
+            target_coords = transform_points_coords(target_coords, projection_type.CARTESIAN2POLAR)
             
             # --- add corrections ---
             target_coords = target_coords + delta 
@@ -56,14 +57,15 @@ class BoundleAdjustment(nn.Module):
         
         proj_coords = torch.cat([patch_coords, elevation_angle], dim = 1)
         
-        T = transform_matrix(poses)
-        proj_coords  = T @ proj_coords
-        proj_coords = transorm_points_coords(proj_coords, projection_type.CARTESIAN2POLAR)
+        # T = transform_matrix(poses)
+        # proj_coords  = T @ proj_coords
+        proj_coords = poses @ proj_coords
+        proj_coords = transform_points_coords(proj_coords, projection_type.CARTESIAN2POLAR)
 
         # calc projection error
-        resiudal = (proj_coords - self.target_coords) * self.weights 
+        residual = (proj_coords - self.target_coords) * self.weights 
     
-        return reisudal 
+        return residual 
 
     def run(self, max_iter, early_stop_tol):
         
@@ -77,11 +79,12 @@ class BoundleAdjustment(nn.Module):
             
             if abs(prev_loss - loss.item()) < early_stop_tol:
                     break
+                
             prev_loss = loss.item()
 
         optimized_poses = self.poses.detach()
         optimized_elevation = self.elevation_angle.detach()
         
-        return optimized_poses, optimized_elevation
+        return optimized_poses.tensor(), optimized_elevation
 
 
