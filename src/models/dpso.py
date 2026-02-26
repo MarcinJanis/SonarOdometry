8950b73e7a9e1126c53f7dcbd40218c952c8a148
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2026-02-26 10:49:48 +0100

    Refactor hidden state initialization and optimization logic

--- src/models/dpso.py
+++ src/models/dpso.py
@@ -37,7 +37,9 @@ class DPSO(nn.Module):
 
 
         self.update_iter = model_cfg.UPDATE_ITERATION
-
+        self.ba_iter = model_cfg.BUNDLE_ADJUSTMENT_ITERATION
+        self.ba_min_err = model.cfg.BUNDLE_ADJUSTMENT_MIN_ERR
+        
         # --- init components --- 
         if mode == 'inference':
             self.train_mode = False
@@ -70,30 +72,46 @@ class DPSO(nn.Module):
             poses, coords_phi = self.PatchGraph.append(x, t, self.device)
             # -- get correlation, contexet and graph edges idx -- 
             corr, ctx, source_frame_idx, target_frame_idx, patch_idx = self.PatchGraph.update_step(poses, coords_phi, self.device)
+            # --- init hidden state --- 
+            n_edges = patch_idx.shape[0]
+            self.h = torch.zeros((n_edges, self.hidden_state_dim), device=self.device, dtype=torch.float)
         else:
             # -- update with new data --
             self.PatchGraph.append(x, t, self.device)
             # -- get correlation, contexet and graph edges idx -- 
             corr, ctx, source_frame_idx, target_frame_idx, patch_idx = self.PatchGraph.update_step(self.device)
-        
-        
+            # --- compose hidden state vector for actual edges --- 
+            self.h = self.PatchGraph.get_hidden_state(patch_idx)
 
-
+        
         # --- Optimize iteration --- 
-
-        # -- init hidden state -- 
-        
-        self.h = torch.zeros((n_edges, self.hidden_state_dim), device=self.device, dtype=torch.float) 
-        # -- iterate -- 
         for _ in range(self.update_iter):
 
             # --- Update operator --- 
-            self.UpdateOperator
-
-            # --- Bundle adjustement --- 
+            self.h, correction = self.UpdateOperator(h, None, corr, ctx, source_frame_idx, target_frame_idx, patch_idx, self.device)
+            delta, weights = correction
             
-            # n_edges = corr_t.shape[0]
-            # h_init = torch.zeros((n_edges, model_config.CONTEXT_OUTPUT_CH), device=device, dtype=torch.float)
+            # --- Bundle adjustement --- 
+            if self.train_mode:
+                BA = BundleAdjustment(poses, PatchGraph.coords_r_theta, coords_phi)
+            else:
+                BA = BundleAdjustment(poses, PatchGraph.patch_coords_r_theta, PatchGraph_i.patch_coords_phi)
+            
+            BA.init_ba(source_frame_idx, patch_idx, delta, weights)
+    
+            opt_poses, opt_elevation = BA.run(max_iter=self.ba_iter, early_stop_tol=self.ba_min_err)
 
-            h, correction = self.UpdateOperator(h, None, corr, ctx, source_frame_idx, target_frame_idx, patch_idx, self.device)
-            delta, weights = correction 
+            # --- Save optimization results --- 
+            # pseudo code: 
+            # poses[source_frame_idx % buff_size] = opt_poses 
+            # patch_state[patch_idx  % buff_size][:, 2] = opt_elevation
+
+
+            # add some mechanizm to extract already optimized poses and points clouds, eg. outside of time window, save to file.
+                
+            
+
+
+
+
+
