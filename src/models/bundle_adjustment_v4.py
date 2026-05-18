f74e40af03dc4e10275eee63b9a7053be4dab4b4
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2026-05-18 10:19:42 +0200

    Refactor pose correction and baseline method
    Refactor pose correction initialization and update baseline method.

--- src/models/bundle_adjustment_v4.py
+++ src/models/bundle_adjustment_v4.py
@@ -33,11 +33,29 @@ class BundleAdjustment(nn.Module):
 
       # poses
       self.init_poses_se3 = pp.SE3(init_poses)
-      self.poses_correction = pp.SE3()
+      poses_correction = torch.zeros((self.b, self.n_total - self.freeze_poses, 7), device = self.device)   
+      poses_correction[:, :, -1] = torch.ones((self.b, self.n_total - self.freeze_poses), device = self.device)            
+      self.poses_correction = pp.Parameters(pp.SE3(poses_correction))
+
+      # patch coords
+      self.patch_coords_r_theta = init_patch_coords_r_theta
+      self.patch_coords_phi = nn.Parameter(init_patch_coords_phi)              
+                     
+  def _get_opt_poses(self):
+      freezed_poses = torch.zeros((self.b, self.freeze_poses, 7), device = self.device)
+      freezed_poses[:, :, -1] = torch.ones((self.b, self.freeze_poses), device = self.device)  
+      poses_correction_full = torch.cat((freezed_poses, self.poses_correction.detach()), dim=1)
+      return self.init_poses_se3 @ poses_correction_full
       
-    
-  def _baseline(self):
-    
+  def _baseline(self, poses, delta):
+
+      patch_coords = torch.cat((self.patch_coords_r_theta, self.patch_coords_phi), dim=-1)
+      
+      source_poses = poses[self.s_idx]
+      target_poses = poses[self.t_idx]
+
+       = project_points()
+      
 
 
 
