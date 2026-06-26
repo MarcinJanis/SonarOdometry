Fix normalization and grid stacking in odometry2d

src/models/odometry2d.py
@@ -81,11 +81,12 @@ class sonar_odometry(nn.Module):
         theta = torch.atan2(x_r, y_r_clamp)
 
         # Nornalization 
-        norm_theta = theta / self.theta_max 
+        norm_theta = theta / (self.theta_max / 2.0)
         norm_r = (r - self.r_min) / (self.r_max - self.r_min) * 2.0 - 1.0
 
         # Crate grid with shape (b, out_h, out_w, 2)
-        grid = torch.stack((norm_theta, norm_r), dim=-1).unsqueeze(0)
+
+        grid = torch.stack((norm_r, norm_theta), dim=-1).unsqueeze(0)
         self.polar2cart_grid = grid.expand(b, -1, -1, -1) 
 
         # crate valid pixels mask 